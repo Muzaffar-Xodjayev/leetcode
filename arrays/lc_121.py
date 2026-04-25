bf75375a19204a2229ac3bcb2842b10d8177632c
Muzaffar <khodjayevmuzaffar@gmail.com>
2026-04-25 22:25:09 +0500

    solve problem

---
+++ arrays/lc_121.py
@@ -0,0 +1,19 @@
+# LeetCode: 121 - Best time to buy and sell Stock
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        profit = 0
+
+        for i in prices[1:]:
+            if i < min_price:
+                min_price = i
+
+            profit = max(profit, i - min_price)
+        return profit
+
+
+s1 = Solution()
+print(s1.maxProfit([7,6,4,3,1]))
